3e52be7dcfce23315539cf3e553ba63755bb688b
Sudhanshu Ranjan <sudhanshuranjangt01@gmail.com>
2024-12-20 12:19:44 +0530

    FIRSTCOMMIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NewProduct from './components/NewProduct';
 
-function App() {
+import Products from './components/Products';
+
+const App = () => {
+  const products = [
+    {
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
+    },
+    { 
+      id: 'p2', 
+      title: 'Sirf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
+    {
+      id: 'p3',
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
+    },
+    {
+      id: 'p4',
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
+
+  function printProductData(data) {
+    console.log("i am inside APP.js")
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewProduct pranay = {printProductData} />
+      <Products items={products} />
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,6 @@
+import'./Item.css'
+function Item(props){
+   const ItemName=props.name ;
+return(<p className="Nirma">{ItemName}</p>);
+}
+export default Item;
